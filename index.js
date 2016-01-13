4eb734b4e18b2c0e2c531fe4ae5a302cf4181ae0
skywickenden <github@skywickenden.co.uk>
2016-01-13 14:29:30 +0000

    Initial validator

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-exports.printMsg = function() {
-  console.log("This is a test message from the checkit-money package");
+/**
+ * value {string} The value that is passed in and being checked if
+ * it is a valid money value.
+ */
+export default function (value, mustIncludeDecimals=false, allowSymbol=false) {
+  const stringValue = value.toString();
+  let valid = true;
+
+  if (mustIncludeDecimals || stringValue.indexOf('.') === -1) {
+    if (mustIncludeDecimals && stringValue.indexOf('.') === -1) {
+      valid = false;
+    } else {
+      const decimals = stringValue.split('.')[1];
+      if (decimals.length > 2 || decimals.length < 2) {
+        valid = false;
+      }
+    }
+  }
+
+  const firstChar = stringValue.charAt(0);
+  if (allowSymbol === false) {
+    if (/^\d+$/.test(firstChar) === false) {
+      valid = false;
+    }
+  } else {
+    if (/^\d+$/.test(firstChar) === false) {
+      stringValue = stringValue.substring(1);
+    }
+  }
+
+  const digits = stringValue.replace('.', '');
+  if (/^\d+$/.test(digits) === false) {
+    valid = false;
+  }
+
+  return valid;
 }
